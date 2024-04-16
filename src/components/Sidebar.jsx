0deee10ab91e7ea2c52af256eecb0476b0308dc7
Iván Mermelstein <ivan.mermelstein@gmail.com>
2024-04-16 08:38:02 -0300

    add sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,27 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <form>
+        <h2>Add an item</h2>
+        <input type="text" />
+        <ActionButton className="btn">Add to list</ActionButton>
+      </form>
+      <section className="button-group">
+        <ActionButton type={"secondary"}>Mark all as complete</ActionButton>
+        <ActionButton type={"secondary"}>Mark all as incomplete</ActionButton>
+        <ActionButton type={"secondary"}>Reset to inital</ActionButton>
+        <ActionButton type={"secondary"}>Remove all items</ActionButton>
+      </section>
+    </div>
+  );
+};
+
+const ActionButton = ({ type, children }) => {
+  return (
+    <button className={`btn ${type === "secondary" ? "btn--secondary" : ""}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Sidebar;
