248c4dc32c0e38d949c3151cf86a8e1ba654d9ce
Iván Mermelstein <ivan.mermelstein@gmail.com>
2024-04-21 19:05:17 -0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ const Footer = () => {
   return (
     <footer>
       Done by{" "}
-      <a href="https://www.linkedin.com/in/ivanmermelstein/" target="_blank">
+      <a
+        style={{ marginLeft: "4px" }}
+        href="https://www.linkedin.com/in/ivanmermelstein/"
+        target="_blank"
+      >
         Iván
       </a>
     </footer>
